1e95277db2661cf0c153d7f9a9eb06560f1b0751
Bunlong Heng <bunlongheng@outlook.com>
2018-07-30 15:33:31 -0400

    Initial Commit

---
+++ tableInTabbed/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  tableInTabbed
+//
+//  Created by Bunlong Heng on 7/30/18.
+//  Copyright © 2018 LR Web Design. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
