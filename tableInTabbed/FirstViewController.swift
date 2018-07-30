736eb1a06bc7f4281ff5d2bebe2a5696f0dad7a0
Bunlong Heng <bunlongheng@outlook.com>
2018-07-30 15:51:52 -0400

    Add Table + Custom Cell

--- tableInTabbed/FirstViewController.swift
+++ tableInTabbed/FirstViewController.swift
@@ -8,7 +8,25 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let animals = ["panda", "dog", "lion"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (animals.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ViewControllerTableViewCell
+//        cell.myImage.image = UIImage(named: animals[indexPath.row] + ".jpg")
+//        cell.myLabel.text = animals[indexPath.row]
+        
+        print(animals[indexPath.row])
+        
+        return cell
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ tableInTabbed/ViewControllerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ViewControllerTableViewCell.swift
+//  tableInTabbed
+//
+//  Created by Bunlong Heng on 7/30/18.
+//  Copyright © 2018 LR Web Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
