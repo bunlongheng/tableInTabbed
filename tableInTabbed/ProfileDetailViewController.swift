15b2f56452eba9d5c4efc3a8b18e4f59ffd589f2
Bunlong Heng <bunlongheng@outlook.com>
2018-07-31 11:12:54 -0400

    add profile detail storyboard and hook UI component with the code

--- tableInTabbed/ProfileDetailViewController.swift
+++ tableInTabbed/ProfileDetailViewController.swift
@@ -9,27 +9,16 @@
 import UIKit
 
 class ProfileDetailViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    
+    @IBOutlet weak var profileName: UILabel!
+    
+    @IBOutlet weak var deviceCount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
 }
